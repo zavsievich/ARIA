e3b9b5581b4512c1da104d6a84d52b62c7d6e525
Zahar Avsievich <zavsievich@zahar-avsievich-u999.wifi.es.local>
2020-03-04 10:52:38 +0300

    add slides

--- components/tabs/script.js
+++ components/tabs/script.js
@@ -1,17 +1,14 @@
 function tabs() {
     const a11y = document.querySelector('.js-a11y');
-    const nona11y = document.querySelector('.js-non-a11y');
+    // const nona11y = document.querySelector('.js-non-a11y');
     const btnsList = Array.from(document.querySelectorAll('.js-btn'));
     const itemsList = Array.from(document.querySelectorAll('.js-item'));
     const expandClass = 'is-expand';
     const activeClass = 'is-active';
-
-    // if (nona11y) {
-    //     nona11yTabs();
-    // } else if (a11y) {
-    //     allyTabs();
-    // }
-
+    const keys = {
+        left: 37,
+        right: 39,
+    };
 
     btnsList.forEach(item => {
         item.addEventListener('click', function () {
@@ -31,32 +28,56 @@ function tabs() {
             item.classList.add(activeClass);
             currentItem.classList.add(expandClass);
 
-            if(a11y) {
+            if (a11y) {
                 item.setAttribute('aria-selected', true);
                 item.setAttribute('tabindex', 0);
             }
         })
     })
 
+    if (a11y) {
+        btnsList.forEach(item => {
+            item.addEventListener('keydown', function (e) {
+                if (e.keyCode == keys.left) {
+                    moveToPrev(e);
+                } else if (e.keyCode == keys.right) {
+                    moveToNext(e);
+                }
+            })
+        })
 
-    // function allyTabs() {
-    //     btnsList.forEach(item => {
-    //         item.addEventListener('click', function () {
-    //             let pressTab = item.getAttribute('data-tab');
+        function moveToPrev(e) {
+            let target = e.target;
+            let currentIndex = btnsList.indexOf(target);
 
-    //             for (let i = 0; i < btnsList.length; i++) {
-    //                 btnsList[i].classList.remove(activeClass);
-    //                 btnsList[i].setAttribute('aria-selected', false);
-    //                 btnsList[i].setAttribute('tabindex', -1);
-    //             }
-    //             item.classList.add(activeClass);
-    //             item.setAttribute('aria-selected', true);
-    //             item.setAttribute('tabindex', 0);
+            if (btnsList[currentIndex - 1]) {
+                btnsList[currentIndex - 1].focus();
+            } else {
+                focusLastTab();
+            }
+        }
 
+        function moveToNext(e) {
+            let target = e.target;
+            let currentIndex = btnsList.indexOf(target);
 
-    //         })
-    //     })
-    // }
+            if (btnsList[currentIndex + 1]) {
+                btnsList[currentIndex + 1].focus();
+            } else {
+                focusFisrtTab();
+            }
+        }
+
+        function focusLastTab() {
+            const lastTab = btnsList.length - 1;
+            btnsList[lastTab].focus();
+        }
+
+        function focusFisrtTab() {
+            const firstTab = 0;
+            btnsList[firstTab].focus();
+        }
+    }
 }
 
 tabs();
